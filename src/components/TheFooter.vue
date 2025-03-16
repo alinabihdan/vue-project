99fd639c1ed274f65db7a500c3ab3b5ff5f6317e
Alina Bihdan <your-email@example.com>
2025-03-16 14:00:19 +0100

    added routes and basic functionality

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>© Copyright 2025 </p>
+    </div>
+  </footer>
+</template>
+
+<style></style>
--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,16 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue()],
+  server: {
+    port: 3000
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
